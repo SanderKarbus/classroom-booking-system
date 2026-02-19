e4389a550e588a6fe1902e935b25629ef30ae7a3
SanderKarbus <Sander.Karbus@vikk.ee>
2026-02-19 12:14:02 +0200

    Change database user and password in get_connection

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return psycopg2.connect(
         host="localhost",
         database="classroom_booking",
-        user="postgres",
-        password="postgres"  # vajadusel muuda
+        user="admin",
+        password="admin"
     )
